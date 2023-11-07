2d026c93d5423c99121daba4fb8ee7bd149adb01
Arsentosh <doncenkoarsenij185@gmail.com>
2023-11-07 17:42:55 +0200

    done

---
+++ src/components/Button/Button.jsx
@@ -0,0 +1,9 @@
+import css from './Button.module.css';
+
+export default function Button({ functions, text }) {
+  return (
+    <button className={css.button} type="button" onClick={functions}>
+      {text}
+    </button>
+  );
+}
